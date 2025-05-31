5ed2a88cb0796e43a33ac3b14e182623145abff6
naitik <naitik.koladiya@gmail.com>
2025-05-31 15:23:27 +0530

    Moved the controllers folder into the server

--- server/routes/stats.js
+++ server/routes/stats.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const authMiddleware = require("../middleware/auth");
-const { getUserStats } = require("../../controllers/statsController");
+const { getUserStats } = require("../controllers/statsController");
 const router = express.Router();
 
 router.get("/user/:userId", authMiddleware, getUserStats);
